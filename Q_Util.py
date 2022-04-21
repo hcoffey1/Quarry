faff5c5724666d8a9bbb9869714c4ba29edbe203
hcoffey1 <hcoffey1@vols.utk.edu>
2022-04-20 20:53:52 -0500

    Refactor function to new file

---
+++ Q_Util.py
@@ -0,0 +1,100 @@
+from qiskit import QuantumCircuit, Aer, execute, IBMQ, transpile
+from qiskit.providers.aer.noise import NoiseModel
+from statistics import mean
+import Eval_Metrics as EM
+
+#0: Use all available cores
+MAX_JOBS=0
+
+def fitness(PST,TVD,Entropy,Swaps):
+    a = 1
+    b = 1
+    c = 1
+    d = 1.0/10
+    fitness = 0
+
+    X = PST*a
+    Y = (TVD*b+Entropy*c+Swaps*d)
+
+    if Y != 0:
+        fitness = X/Y
+
+    return fitness
+
+def getMaxQubit(cm):
+    maxQubit = 0
+    maxX = (sorted(cm, key=lambda x: x[0], reverse=True)[0][0])
+    maxY = (sorted(cm, key=lambda x: x[1], reverse=True)[1][1])
+    return max(maxX, maxY)
+
+def getAverageDegree(cm):
+    maxQubit = getMaxQubit(cm)
+
+    counts = [0] * (maxQubit+1)
+
+    for e in cm:
+        counts[e[0]] += 1
+        counts[e[1]] += 1
+
+    return mean(counts) 
+
+def getGateCounts(qc, basisGates):
+    """Get counts of each gate type in the given circuit"""
+
+    gateCounts = {}
+
+    for g in basisGates:
+        gateCounts[g] = 0
+    
+    gateCounts["measure"] = 0
+
+    qasm = qc.qasm()
+
+    for l in qasm.split('\n'):
+        if len(l) == 0:
+            continue
+
+        if "barrier" in l \
+                or "QASM" in l \
+                or "qreg" in l \
+                or "creg" in l \
+                or "include" in l:
+                    continue
+        gate = l.split()[0].split('(')[0]
+        if gate not in gateCounts:
+            print("Unhandled instruction: ", l)
+            exit(1)
+
+        gateCounts[gate] += 1
+
+    return gateCounts
+
+def simCircuit(qc, backend):
+    '''Run circuit on simulated backend and collect result metrics'''
+
+    backendName = backend.configuration().backend_name
+    basisGates = backend.configuration().basis_gates
+    print(backendName)
+
+    if "swap" not in basisGates:
+        basisGates = basisGates + ["swap"]
+
+    swap_qc = transpile(qc, basis_gates=basisGates, optimization_level=0, backend=backend)
+
+    swapCount = getGateCounts(swap_qc, basisGates)['swap']
+
+    ideal_result = execute(
+        qc, backend=Aer.get_backend('qasm_simulator'), max_parallel_threads=MAX_JOBS).result()
+    noisy_result = execute(qc, backend=backend, max_parallel_threads=MAX_JOBS).result()
+
+    PST = EM.Compute_PST(correct_answer=ideal_result.get_counts(
+    ).keys(), dict_in=noisy_result.get_counts())
+
+    TVD = EM.Compute_TVD(dict_ideal=ideal_result.get_counts(
+    ), dict_in=noisy_result.get_counts())
+
+    Entropy = EM.Compute_Entropy(dict_in=noisy_result.get_counts())
+
+    Fitness = fitness(PST, TVD, Entropy, swapCount)
+
+    return [PST, TVD, Entropy, swapCount, Fitness]
--- est.py
+++ est.py
@@ -1,86 +1,29 @@
 from ctypes.wintypes import MAX_PATH
 from qiskit import QuantumCircuit, Aer, execute, IBMQ
-import Eval_Metrics as EM
 import sys
 import time
 import os
 from os.path import exists
 from qiskit.providers.aer.noise import NoiseModel
 from qiskit import transpile
+import Eval_Metrics as EM
 
-#0: Use all available cores
-MAX_JOBS=0
+from Q_Util import simCircuit
 
 #Mockup backends
 import qiskit.test.mock.backends as BE
 
-def getGateCounts(qc, basisGates):
-    """Get counts of each gate type in the given circuit"""
 
-    gateCounts = {}
-
-    for g in basisGates:
-        gateCounts[g] = 0
-    
-    gateCounts["measure"] = 0
-
-    qasm = qc.qasm()
-
-    for l in qasm.split('\n'):
-        if len(l) == 0:
-            continue
-
-        if "barrier" in l \
-                or "QASM" in l \
-                or "qreg" in l \
-                or "creg" in l \
-                or "include" in l:
-                    continue
-        gate = l.split()[0].split('(')[0]
-        if gate not in gateCounts:
-            print("Unhandled instruction: ", l)
-            exit(1)
-
-        gateCounts[gate] += 1
-
-    return gateCounts
-
-def simCircuit(resultDict, qc, backend):
+def evalCircuit(resultDict, qc, backend):
     '''Run circuit on simulated backend and collect result metrics'''
 
     backendName = backend.configuration().backend_name
-    basisGates = backend.configuration().basis_gates
-    print(backendName)
-
-    if "swap" not in basisGates:
-        basisGates = basisGates + ["swap"]
-
-    swap_qc = transpile(qc, basis_gates=basisGates, optimization_level=0, backend=backend)
-
-    swapCount = getGateCounts(swap_qc, basisGates)['swap']
-
-    ideal_result = execute(
-        qc, backend=Aer.get_backend('qasm_simulator'), max_parallel_threads=MAX_JOBS).result()
-    noisy_result = execute(qc, backend=backend, max_parallel_threads=MAX_JOBS).result()
-
-    PST = EM.Compute_PST(correct_answer=ideal_result.get_counts(
-    ).keys(), dict_in=noisy_result.get_counts())
-
-    TVD = EM.Compute_TVD(dict_ideal=ideal_result.get_counts(
-    ), dict_in=noisy_result.get_counts())
-
-    #IST = EM.Compute_IST(correct_answer=ideal_result.get_counts(
-    #).keys(), dict_in=noisy_result.get_counts())
-
-    Entropy = EM.Compute_Entropy(dict_in=noisy_result.get_counts())
-
-    Fitness = fitness(PST, TVD, Entropy, swapCount)
 
     if qc.name not in resultDict:
         resultDict[qc.name] = []
 
-    resultDict[qc.name].append([backendName, [
-        PST, TVD, Entropy, swapCount, Fitness]])
+    resultDict[qc.name].append([backendName, simCircuit(qc, backend)])
+
 
 def simCircuitIBMQ(resultDict, qc, backend):
     '''Run circuit on simulated backend and collect result metrics'''
@@ -90,7 +33,8 @@ def simCircuitIBMQ(resultDict, qc, backend):
 
     ideal_result = execute(
         qc, backend=Aer.get_backend('qasm_simulator'), max_parallel_threads=MAX_JOBS).result()
-    noisy_result = execute(qc, backend=Aer.get_backend('qasm_simulator'), noise_model=nm, max_parallel_threads=MAX_JOBS).result()
+    noisy_result = execute(qc, backend=Aer.get_backend(
+        'qasm_simulator'), noise_model=nm, max_parallel_threads=MAX_JOBS).result()
 
     PST = EM.Compute_PST(correct_answer=ideal_result.get_counts(
     ).keys(), dict_in=noisy_result.get_counts())
@@ -145,20 +89,6 @@ def printResults(resultDict, execTime):
             print("{:20}{:<10.3f}{:<10.3f}{:<10.3f}{:<10}{:<10.3f}".format(
                 backend, PST, TVD, Entropy, swapCount, Fitness))
 
-def fitness(PST,TVD,Entropy,Swaps):
-    a = 1
-    b = 1
-    c = 1
-    d = 1.0/10
-    fitness = 0
-
-    X = PST*a
-    Y = (TVD*b+Entropy*c+Swaps*d)
-
-    if Y != 0:
-        fitness = X/Y
-
-    return fitness
 
 def main():
     if len(sys.argv) == 1:
@@ -176,17 +106,18 @@ def main():
 
     if TOKEN_FILE != None and exists(TOKEN_FILE):
         with open(TOKEN_FILE) as f:
-            TOKEN=f.read()
+            TOKEN = f.read()
 
         print("Token found, using pulling IBMQ data...", end='')
         IBMQ.save_account(TOKEN, overwrite=True)
         IBMQ.load_account()
         provider = IBMQ.providers()[0]
         backendsIBMQ = provider.backends()
-        backendsIBMQ = list(filter(lambda backend: "simulator" not in backend.configuration().backend_name, backendsIBMQ))
+        backendsIBMQ = list(filter(
+            lambda backend: "simulator" not in backend.configuration().backend_name, backendsIBMQ))
         print("Done")
 
-        #For writing noise models out to compare 
+        #For writing noise models out to compare
         #nm = NoiseModel.from_backend(backends[0])
         #nm = NoiseModel.from_backend(FakeArmonk())
         #with open("armonk_04_18_22", 'w') as f:
@@ -208,7 +139,7 @@ def main():
             lambda backend: backend.configuration().n_qubits >= qc.num_qubits, backends))
 
         #Only using first n backends to speed up testing
-        n = 3
+        n = 10
         backends = backends[:n]
 
     #for b in backends:
@@ -221,7 +152,7 @@ def main():
     resultDict = {}
     if backendsIBMQ == None:
         for backend in backends:
-            simCircuit(resultDict, qc, backend)
+            evalCircuit(resultDict, qc, backend)
     else:
         for backend in backendsIBMQ:
             if backend.configuration().n_qubits < qc.num_qubits:
