2fcc04b0b995f2047e8cdeff63b8528ea761d55c
hcoffey1 <hcoffey1@vols.utk.edu>
2022-05-08 05:17:35 -0500

    Add graph methods

--- src/QUtil.py
+++ src/QUtil.py
@@ -1,3 +1,4 @@
+from numpy import average
 from qiskit import Aer, execute, transpile, transpiler, QuantumCircuit
 from qiskit.providers.aer.noise import NoiseModel
 from pandas import DataFrame
@@ -9,19 +10,73 @@ import networkx
 import os
 import inspect
 import sys
+import matplotlib.pyplot as plt
 
 currentdir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-import qasm.QASMBench.metrics.OpenQASMetric as QB
 
 #Mockup backends
 import qiskit.test.mock.backends as BE
+import qasm.QASMBench.metrics.OpenQASMetric as QB
+
 
 #0: Use all available cores
 MAX_JOBS = 24
 GLOBAL_BASIS_GATES = None
+
+
+def drawWeightedGraph(G):
+    """Visualize weighted Networkx graph"""
+    pos = networkx.spring_layout(G)
+    networkx.draw(G, pos, with_labels=True)
+    labels = networkx.get_edge_attributes(G, 'weight')
+    networkx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    fig = plt.gcf()
+    fig.suptitle(G.name)
+    plt.show()
+
+
+def getCxGraph(qc):
+    """Nodes are Qubits, Edges are CX, Weights are Qubit number distances"""
+    G = networkx.Graph()
+    G.name = qc.name
+    for instruction, qargs, cargs in qc._data:
+        if len(qargs) > 1:
+            qb = ((qargs[0]._index), (qargs[1]._index))
+            G.add_edge(qb[0], qb[1], weight=abs(qb[0]-qb[1]))
+    return G
+
+
+def getGraphMetrics(G, label):
+    """Calculate metrics from Networkx graph"""
+    output = {}
+    output["{}GraphDensity".format(label)] = networkx.density(G)
+    output["{}AvgConnectivity".format(
+        label)] = networkx.average_node_connectivity(G)
+    output["{}AvgNeighborDegree".format(label)] = mean(
+        list(networkx.average_neighbor_degree(G).values()))
+    output["{}AvgClustering".format(label)] = networkx.average_clustering(G)
+    output["{}AvgShortestPath".format(
+        label)] = networkx.average_shortest_path_length(G)
+
+    return output
+
+
+def getCxGraphMetrics(G: networkx.Graph):
+    """Calculate graph metrics and weight metrics"""
+    output = getGraphMetrics(G, 'CX')
+
+    weights = []
+    for e in G.edges:
+        weights.append(G[e[0]][e[1]]['weight'])
+
+    output['CXAverageWeight'] = average(weights)
+    output['CXMaxWeight'] = max(weights)
+    output['CXMinWeight'] = min(weights)
+
+    return output
 
 
 def getAvgMeasurementSuccess(noise: NoiseModel) -> float:
@@ -41,7 +96,6 @@ def getAvgMeasurementSuccess(noise: NoiseModel) -> float:
 
 
 def getAvgGateSuccess(noise: NoiseModel) -> dict:
-
     gateSuccess = {}
 
     for gate in GLOBAL_BASIS_GATES:
@@ -138,14 +192,11 @@ def getV2Input(qc: QuantumCircuit, backend) -> DataFrame:
     graph.add_edges_from(coupling_map)
 
     output["Machine"] = MachineDict[name]
+    output["QCAvgDegree"] = getAverageDegree(coupling_map)
 
-    output["GraphDensity"] = networkx.density(graph)
-    output["AvgDegree"] = getAverageDegree(coupling_map)
-    output["AvgConnectivity"] = networkx.average_node_connectivity(graph)
-    output["AvgNeighborDegree"] = mean(
-        list(networkx.average_neighbor_degree(graph).values()))
-    output["AvgClustering"] = networkx.average_clustering(graph)
-    output["AvgShortestPath"] = networkx.average_shortest_path_length(graph)
+    output = {**output, **(getGraphMetrics(graph, 'QC'))}
+
+    output = {**output, **(getCxGraphMetrics(getCxGraph(qc)))}
 
     #Circuit Metrics
     output["NumQubit"] = numQubit
@@ -188,31 +239,30 @@ def getGateCounts(qc, basisGates):
 
     gateCounts = {}
 
+    ignoredInstructions = ['barrier']
+
     for g in basisGates:
         gateCounts[g] = 0
 
     gateCounts["measure"] = 0
 
-    qasm = qc.qasm()
+    for instruction, qargs, cargs in qc._data:
+        if len(qargs) > 1:
+            qb = ((qargs[0]._index), (qargs[1]._index))
+        else:
+            qb = (qargs[0]._index, )
 
-    for l in qasm.split('\n'):
-        if len(l) == 0:
+        if instruction.name in ignoredInstructions:
             continue
 
-        if "barrier" in l \
-                or "QASM" in l \
-                or "qreg" in l \
-                or "creg" in l \
-                or "include" in l:
-            continue
-        gate = l.split()[0].split('(')[0]
-        if gate not in gateCounts:
-            print("Unhandled instruction: ", l)
+        if instruction.name not in gateCounts:
+            print("Unhandled instruction: ", instruction.name)
             raise RuntimeError
 
-        gateCounts[gate] += 1
+        gateCounts[instruction.name] += 1
 
     return gateCounts
+
 
 def getSwapCount(qc, backend):
     '''Get count of SWAP operations added for circuit on given backend.'''
@@ -228,6 +278,7 @@ def getSwapCount(qc, backend):
 
     return getGateCounts(swap_qc, basisGates)['swap']
 
+
 def simCircuit(qc, backend):
     '''Run circuit on simulated backend and collect result metrics'''
 
@@ -242,7 +293,6 @@ def simCircuit(qc, backend):
     noisy_result = execute(qc, backend=backend,
                            max_parallel_threads=MAX_JOBS).result()
 
-
     outDict["PST"] = EM.computePST(correct_answer=ideal_result.get_counts(
     ).keys(), dict_in=noisy_result.get_counts())
 
@@ -251,7 +301,8 @@ def simCircuit(qc, backend):
 
     outDict["Entropy"] = EM.computeEntropy(dict_in=noisy_result.get_counts())
 
-    outDict["L2"] = EM.computeL2(noisy_result.get_counts(), ideal_result.get_counts())
+    outDict["L2"] = EM.computeL2(
+        noisy_result.get_counts(), ideal_result.get_counts())
     outDict["Hellinger"] = EM.computeHellinger(
         noisy_result.get_counts(), ideal_result.get_counts())
 
